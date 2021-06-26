6c7dac3326717ba1a6260936054c8c310c488ed4
Parvez Ahmed <miazyparvez@gmail.com>
2021-06-27 02:13:22 +0600

    context api

---
+++ src/component/Mycontext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mycontext = React.createContext();
+const Dadaprovider = Mycontext.Provider;
+const PutiConsumer = Mycontext.Consumer;
+
+export { Dadaprovider, PutiConsumer };
